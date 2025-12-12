fa85402850ba3573fab9725cfeb1641597f80fbd
Eve435-IV <SnowOut435@gmail.com>
2025-12-12 14:51:58 +0700

    12/12/25

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -25,7 +25,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home Page",
+          title: "Home",
           tabBarIcon: ({ color, focused }) => {
             return focused ? (
               <Ionicons name="home" size={24} color={color} />
