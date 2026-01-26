849ca2ba2c4ff868b53db7c713afdfc37bfd4167
Sreynet-long <sreynetlong168@gmail.com>
2026-01-26 15:28:55 +0700

    1/26/26

--- config/env.js
+++ config/env.js
@@ -1,6 +1,7 @@
 //LIVE
-// export const FILE_BASE_URL =
-//   "https://endpoint-goglobal-online-learning.go-globalit.com/api";
+export const FILE_BASE_URL =
+  "https://endpoint-goglobal-online-learning.go-globalit.com/api";
 
 //LOCAL
-export const FILE_BASE_URL = "http://192.168.5.36:5210/api";
+// export const FILE_BASE_URL = "http://192.168.5.36:5210/api";
+s
