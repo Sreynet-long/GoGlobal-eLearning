6c9257c176a0147d3294c4dda31bf32542bd6f4d
Sreynet-long <sreynetlong168@gmail.com>
2026-01-27 13:55:55 +0700

    1/27/26

--- app/auth/GlobalUtils/UploadImage.jsx
+++ app/auth/GlobalUtils/UploadImage.jsx
@@ -1,6 +1,5 @@
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import * as ImagePicker from "expo-image-picker";
-import { Alert, StyleSheet, TouchableOpacity } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 
 const COLORS = {
   primary: "#25375A",
@@ -9,28 +8,8 @@ const COLORS = {
 };
 
 export default function UploadImage({ setFileUpload }) {
-  const pickImages = async () => {
-    const permission = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (!permission.granted) {
-      Alert.alert("Permission required", "Please allow photo access");
-      return [];
-    }
-
-    const result = await ImagePicker.launchImageLibraryAsync({
-      // mediaTypes: ImagePicker.MediaType.Image,
-      mediaTypes: ["images"],
-      quality: 1,
-    });
-
-    if (result?.canceled) return [];
-
-    // ✅ Always return array
-    return result?.assets ?? [];
-  };
-
   return (
-    <TouchableOpacity style={styles.camera} >
+    <TouchableOpacity style={styles.camera}>
       <MaterialIcons name="photo-camera" size={20} color={COLORS.white} />
     </TouchableOpacity>
   );
